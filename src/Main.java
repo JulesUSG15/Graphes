7c81c59b9b2afb788017c84f266d0ae7f6a910bf
Dorian <dorian.tonnis@hotmail.fr>
2024-05-13 09:23:05 +0200

    exo2

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,41 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "graphe-oriente-02.gra";
-
         try {
             // === Exo 1 ===
 
 
             // === Exo 2 ===
-            
+            System.out.println("=== Exo 2 ===");
+            Graph graph = new Graph("graphe-oriente-02.gra");
+            System.out.println("Nombre de chemins possibles de r à s: " + nbCheminVersSommet(graph, 9, 10));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-}
 
+
+    // === Exo 1 ===
+
+
+    // === Exo 2 ===
+
+    static int nbCheminVersSommet(Graph graph, int startId, int endId) {
+        if(startId == endId) { // Si on est arrivé à la destination, on a trouvé un chemin
+            return 1;
+        }
+        else {
+            List<Integer> neighbors = graph.getNeighbors(startId);
+            if(neighbors.isEmpty()) { // Si on est bloqué, on ne peut pas atteindre la destination
+                return 0;
+            }
+            else {
+                int nbChemin = 0;
+                for(int neighbor : neighbors) { // somme des chemins possibles pour chaque voisin
+                    nbChemin += nbCheminVersSommet(graph, neighbor, endId);
+                }
+                return nbChemin;
+            }
+        }
+    }
+}
